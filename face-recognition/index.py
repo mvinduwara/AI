ff4d776d7105ef2d7962665be3a89abe117b313a
mvinduwara <mvinduwara@gmail.com>
2026-06-23 23:47:11 +0530

    Update index.py

--- face-recognition/index.py
+++ face-recognition/index.py
@@ -1,7 +1,17 @@
 import cv2
 
-i = cv2.imread('image1.png')
-cv2.imshow('image', i)
+d = face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+
+originalImage = cv2.imread('image1.png')
+x = cv2.cvtColor(originalImage, cv2.COLOR_RGB2GRAY)
+
+face = d.detectMultiScale(x, scaleFactor=1.1, minNeighbors=5)
+
+for(x,y,w,h) in face:
+    cv2.rectangle(originalImage , (x,y) , (x+w,y+h) , (0,0,255) , 2)
+
+cv2.imshow('image', originalImage)
 cv2.waitKey(0)
 
 # print(cv2.__version__)
